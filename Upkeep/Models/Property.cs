488178b3da1324a26dce907af7e0dd4281292262
Dakota <dakota.c.upchurch@gmail.com>
2021-07-23 16:14:00 -0500

    some initial file setup

---
+++ Upkeep/Models/Property.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Upkeep.Models
+{
+    public class Property
+    {
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string address { get; set; }
+
+        [Required]
+        public int serviceCharge { get; set; }
+
+        [Required]
+        public DateTime lastService { get; set; }
+
+        public string notes { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        [MaxLength(255)]
+        public string image { get; set; }
+
+        public int UserId { get; set; }
+        public User user { get; set; }
+
+
+    }
+}
---
+++ Upkeep/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Upkeep.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(555)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        public int Phone { get; set; }
+    }
+}
