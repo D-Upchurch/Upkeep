197a55dc36c7aaed2f871410ffafcd32628b103a
Dakota <dakota.c.upchurch@gmail.com>
2021-07-26 16:04:01 -0500

    equipment repositories and controller built out, will need to test later/tomorrow

---
+++ Upkeep/Repositories/IEquipmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Upkeep.Models;
+
+namespace Upkeep.Repositories
+{
+    public interface IEquipmentRepository
+    {
+        void Add(Equipment equipment);
+        void Delete(int id);
+        Equipment GetEquipmentById(int id);
+        List<Equipment> GetEquipmentByUserId(int userId);
+        void Update(Equipment equipment);
+    }
+}
