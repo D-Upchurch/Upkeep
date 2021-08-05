3a713f3caf463855ff7de32c5b4582b4e0c39f2e
Dakota <dakota.c.upchurch@gmail.com>
2021-08-05 16:14:15 -0500

    fixed delete bug from home page

--- Upkeep/client/src/components/Home.js
+++ Upkeep/client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container, Spinner } from "reactstrap";
-import { filterWeekly } from "../modules/transactionManager";
-import { filterProperties } from "../modules/propertyManager";
+import { filterWeekly, deleteTransaction } from "../modules/transactionManager";
+import { filterProperties, deleteProperty } from "../modules/propertyManager";
 import Transaction from "./Transaction/TransactionCard";
 import Property from "./Property/PropertyCard";
 import { useHistory } from "react-router";
@@ -24,6 +24,22 @@ export const Home = () => {
         history.push("/Transaction")
     }
 
+    const handleDeleteProperty = (id) => {
+        let yes = window.confirm("Are you sure you want to delete this property?")
+        if (yes === true) {
+            deleteProperty(id)
+            return fetchFilterProperties();
+        }
+    };
+
+    const handleDeleteTransaction = (id) => {
+        let yes = window.confirm("Are you sure you want to delete this transaction?")
+        if (yes === true) {
+            deleteTransaction(id)
+            return fetchFilterWeekly();
+        }
+    };
+
     useEffect(() => {
         fetchFilterWeekly();
         fetchFilterProperties();
@@ -33,27 +49,27 @@ export const Home = () => {
         <>
             <div className="home-flex">
                 <div>
-                    <span>Properties Needing Service</span>
+                    <h2>Properties Needing Service</h2>
                     <div className="container">
                         <div className="row justify-content-center">
 
                             {properties.map((property) => (
-                                <Property property={property} key={property.id} />
+                                <Property property={property} key={property.id} handleDeleteProperty={handleDeleteProperty} />
                             ))}
 
                         </div>
                     </div>
                 </div>
                 <div>
-                    <span>Current Weekly Transactions</span>
+                    <h2>Current Weekly Transactions</h2>
                     <div className="container">
                         <div className="row justify-content-center">
 
                             {transactions.map((transaction) => (
-                                <Transaction transaction={transaction} key={transaction.id} />
+                                <Transaction transaction={transaction} key={transaction.id} handleDeleteTransaction={handleDeleteTransaction} />
                             ))}
 
-                            <Button id="greenButton" className="btn btn-primary" onClick={handleAllTransactions}>All Transactions</Button>
+                            <Button id="greenButton" className="btn btn-primary" onClick={handleAllTransactions} >All Transactions</Button>
                         </div>
                     </div>
                 </div>
